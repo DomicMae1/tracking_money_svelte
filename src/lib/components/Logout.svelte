071c5219d59802d293cf48879c6e6fe5edf0352f
DomicMae1 <ardon.yunors@tako.co.id>
2025-09-20 14:56:25 +0700

    feat(logout): add logout component

---
+++ src/lib/components/Logout.svelte
@@ -0,0 +1,16 @@
+<script>
+	function logout() {
+		// Hapus cookie token
+		document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+
+		// Redirect ke halaman login
+		window.location.href = '/login';
+	}
+</script>
+
+<button
+	class="absolute top-4 right-4 rounded-lg bg-red-500 px-4 py-2 text-white shadow-md hover:bg-red-600"
+	on:click={logout}
+>
+	Logout
+</button>
